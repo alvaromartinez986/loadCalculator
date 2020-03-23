137b49e369eec8a85bdb6fb20dddc02da7442135
Alvaro Martinez <amartine@pros.com>
2020-03-23 13:45:10 -0500

    Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
+
+function calculateResults() {
+    // Ui Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayment = parseFloat(years.value) * 12;
+
+    // Compute monthly payment 
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayment);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayment).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayment) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+    } else {
+        showError('please check your numbers');
+    }
+    document.getElementById('loading').style.display = 'none';
+}
